05cd3c6f854f57f6fb84a97f6a76406a3cf21928
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2024-08-30 14:27:42 -0300

    adicionando get no serializer do usuario

--- volpi_api_agua/serializers.py
+++ volpi_api_agua/serializers.py
@@ -1,22 +1,34 @@
 from rest_framework import serializers
 from volpi_api_agua.models import Usuario, AguaIngerida
+from django.db.models import Sum
+from django.db import models
+from django.utils import timezone
 
 class UsuarioSerializer(serializers.ModelSerializer):
+    meta_diaria = serializers.SerializerMethodField()
+    total_agua_ingerida = serializers.SerializerMethodField()
+    quantidade_faltante = serializers.SerializerMethodField()
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ['id', 'nome', 'peso', 'meta_diaria', 'total_agua_ingerida', 'quantidade_faltante']
+
+    def get_meta_diaria(self, obj):
+        # Calcula a meta diária com base no peso do usuário
+        return obj.peso * 35
+
+    def get_total_agua_ingerida(self, obj):
+        # Calcula a quantidade total de água ingerida hoje data atual
+        today = timezone.now().date()
+        total = AguaIngerida.objects.filter(usuario=obj, data=today).aggregate(total=Sum('qtd_agua'))['total'] or 0
+        return total
+    
+    def get_quantidade_faltante(self, obj):
+        # Calcula quanto falta para atingir a meta diária subtraindo a meta pela ingestão
+        return self.get_meta_diaria(obj) - self.get_total_agua_ingerida(obj)
 
 class AguaIngeridaSerializer(serializers.ModelSerializer):
     class Meta:
         model = AguaIngerida
         fields = ['id', 'usuario', 'qtd_agua', 'data', 'meta_diaria', 'total_agua_ingerida', 'quantidade_faltante']
         #fields = '__all__' # Não estava retornando os gets #
-
-    def get_meta_diaria(self, obj):
-        return obj.meta_diaria()
-
-    def get_total_agua_ingerida(self, obj):
-        return obj.total_agua_ingerida()
-
-    def get_quantidade_faltante(self, obj):
-        return obj.quantidade_faltante()        
+ 
